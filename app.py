1353a442c35e8a0de49c9360d6e86f5a505cc415
Muhammad <mbasher1010@gmail.com>
2025-11-30 21:51:10 -0500

    Added clubs page

--- app.py
+++ app.py
@@ -5,5 +5,8 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+def clubs():
+    return render_template('clubs.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
