e43e581ae920c4f7102bd89d34dd81838d63d93f
Muhammad <mbasher1010@gmail.com>
2025-12-01 21:48:21 -0500

    Added json club data support for clubs page

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 from flask import Flask, render_template
+import json
+
+with open('data/clubs.json', 'r') as file:
+    data = json.load(file)
+
+print(data)
+print(type(data))
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,3 +19,12 @@ def clubs():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/search')
+def search():
+    clubTables = ("Name", "Category", "Contact", "Description", "Tags")
+
+    with open('data/clubs.json') as f:
+        clubData = json.load(f)
+
+        return render_template('templates/clubs.html', clubData = clubData, clubTables = clubTables)
