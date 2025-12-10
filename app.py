dc19d1d8cdfefcdc86383c0fe61b98f07d5c91b9
Muhammad <mbasher1010@gmail.com>
2025-12-09 21:29:33 -0500

    Added support for club, survey, and join pages

--- app.py
+++ app.py
@@ -1,30 +1,28 @@
 from flask import Flask, render_template
 import json
 
-with open('data/clubs.json', 'r') as file:
-    data = json.load(file)
-
-print(data)
-print(type(data))
-
-
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+@app.route('/clubs')
 def clubs():
+    # clubTables = ("Name", "Category", "Contact", "Description", "Tags")
+
+    # with open('data/clubs.json') as f:
+    #     clubData = json.load(f)
+
     return render_template('clubs.html')
+
+@app.route('/survey')
+def survey():
+    return render_template('survey.html')
+
+@app.route('/join')
+def join():
+    return render_template('join.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-@app.route('/search')
-def search():
-    clubTables = ("Name", "Category", "Contact", "Description", "Tags")
-
-    with open('data/clubs.json') as f:
-        clubData = json.load(f)
-
-        return render_template('templates/clubs.html', clubData = clubData, clubTables = clubTables)
